8036c67667cb625ac5b602de02512b3a270aa229
Elvis Hew <com.elvishew@gmail.com>
2016-08-21 23:05:20 +0800

    Auto deploy

---
+++ .buildscript/deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Deploy to Sonatype's repo.
+#
+# Adapted from https://github.com/JakeWharton/butterknife/blob/master/.buildscript/deploy_snapshot.sh 
+
+SLUG="elvishew/xLog"
+JDK="oraclejdk8"
+BRANCH="master"
+
+set -e
+
+if [ "$TRAVIS_REPO_SLUG" != "$SLUG" ]; then
+  echo "Skipping snapshot deployment: wrong repository. Expected '$SLUG' but was '$TRAVIS_REPO_SLUG'."
+elif [ "$TRAVIS_JDK_VERSION" != "$JDK" ]; then
+  echo "Skipping snapshot deployment: wrong JDK. Expected '$JDK' but was '$TRAVIS_JDK_VERSION'."
+elif [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo "Skipping snapshot deployment: was pull request."
+elif [ "$TRAVIS_BRANCH" != "$BRANCH" ]; then
+  echo "Skipping snapshot deployment: wrong branch. Expected '$BRANCH' but was '$TRAVIS_BRANCH'."
+else
+  echo "Deploying..."
+  ./gradlew uploadArchives
+  echo "Deployed!"
+fi
