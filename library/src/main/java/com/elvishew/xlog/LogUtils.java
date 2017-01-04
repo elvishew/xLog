90cb1c803cb52f70a48e82d760045605e82cfcf8
Elvis Hew <com.elvishew@gmail.com>
2017-01-04 15:12:43 +0800

    Support compressing log files to a zip file

--- library/src/main/java/com/elvishew/xlog/LogUtils.java
+++ library/src/main/java/com/elvishew/xlog/LogUtils.java
@@ -16,6 +16,15 @@
 
 package com.elvishew.xlog;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
 import static com.elvishew.xlog.XLog.assertInitialization;
 
 /**
@@ -89,5 +98,75 @@ public class LogUtils {
   public static String addBorder(String[] segments) {
     assertInitialization();
     return XLog.sLogConfiguration.borderFormatter.format(segments);
+  }
+
+  private static final int BUFFER_SIZE = 8 * 1024; // 8K
+
+  /**
+   * Compress all files under the specific folder to a single zip file.
+   * <p>
+   * Should be call in background thread.
+   *
+   * @param folderPath  the specific folder path
+   * @param zipFilePath the zip file path
+   * @throws IOException if any error occurs
+   * @since 2.14.0
+   */
+  public static void compress(String folderPath, String zipFilePath) throws IOException {
+    File folder = new File(folderPath);
+    if (!folder.exists() || !folder.isDirectory()) {
+      throw new IOException("Folder " + folderPath + " does't exist or isn't a directory");
+    }
+
+    File zipFile = new File(zipFilePath);
+    if (!zipFile.exists()) {
+      File zipFolder = zipFile.getParentFile();
+      if (!zipFolder.exists()) {
+        if (!zipFolder.mkdirs()) {
+          throw new IOException("Zip folder " + zipFolder.getAbsolutePath() + " not created");
+        }
+      }
+      if (!zipFile.createNewFile()) {
+        throw new IOException("Zip file " + zipFilePath + " not created");
+      }
+    }
+
+    BufferedInputStream bis;
+    ZipOutputStream zos = new ZipOutputStream(
+        new BufferedOutputStream(new FileOutputStream(zipFile)));
+    try {
+      byte buffer[] = new byte[BUFFER_SIZE];
+      for (String fileName : folder.list()) {
+        if (fileName.equals(".") || fileName.equals("..")) {
+          continue;
+        }
+
+        File file = new File(folder, fileName);
+        if (!file.isFile()) {
+          continue;
+        }
+
+        FileInputStream fis = new FileInputStream(file);
+        bis = new BufferedInputStream(fis, BUFFER_SIZE);
+        try {
+          ZipEntry entry = new ZipEntry(fileName);
+          zos.putNextEntry(entry);
+          int count;
+          while ((count = bis.read(buffer, 0, BUFFER_SIZE)) != -1) {
+            zos.write(buffer, 0, count);
+          }
+        } finally {
+          try {
+            bis.close();
+          } catch (IOException e) {
+          }
+        }
+      }
+    } finally {
+      try {
+        zos.close();
+      } catch (IOException e) {
+      }
+    }
   }
 }
